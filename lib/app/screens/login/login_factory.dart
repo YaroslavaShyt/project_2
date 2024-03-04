0aa914558d9edb294032dbf8aa1b8d639f77ef1e
Yaroslava Shyt <yaroslavashyt@gmail.com>
2024-03-04 14:08:57 +0200

    - removed unused imports

--- lib/app/screens/login/login_factory.dart
+++ lib/app/screens/login/login_factory.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:project_2/app/routing/inavigation_util.dart';
 import 'package:project_2/app/screens/login/login_screen.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project_2/app/app.dart';
@@ -10,7 +9,6 @@ import 'package:project_2/app/services/get_it/get_it.dart';
 import 'package:project_2/domain/services/iauth_service.dart';
 import 'package:project_2/domain/services/iuser_service.dart';
 import 'package:project_2/app/services/user/user_service.dart';
-import 'package:project_2/data/login/login_repository.dart';
 import 'package:project_2/domain/login/ilogin_repository.dart';
 import 'package:project_2/firebase_options.dart';
 import 'package:provider/provider.dart';
@@ -27,11 +25,8 @@ void main() async {
   final INavigationUtil navigationUtil = NavigationUtil();
   final AppRouter appRouter = AppRouter();
 
-  final ILoginRepository loginRepository =
-      LoginRepository(firebaseAuth: FirebaseAuth.instance);
   final IAuthService authService =
-      AuthService(loginRepository: loginRepository);
-
+      AuthService(loginRepository: getItInst<ILoginRepository>());
   final IUserService userService = UserService();
 
   runApp(MultiProvider(
