1065a778a9bb49e89e857cde3d9f447bb0853ef2
Yaroslava Shyt <yaroslavashyt@gmail.com>
2024-03-17 17:38:38 +0200

    - cloud messaging

--- lib/app/screens/plants_home/plants_home_view_model.dart
+++ lib/app/screens/plants_home/plants_home_view_model.dart
@@ -1,16 +1,12 @@
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart';
 import 'package:project_2/app/common/base_change_notifier.dart';
 import 'package:project_2/app/routing/inavigation_util.dart';
 import 'package:project_2/app/services/networking/firebase_storage/storage_service.dart';
-import 'package:project_2/data/storage/firebase_storage_repository.dart';
-import 'package:project_2/app/services/networking/firebase_storage/paths.dart';
 import 'package:project_2/app/utils/permissions/permission_handler.dart';
 import 'package:project_2/data/plants/plants_data.dart';
 import 'package:project_2/domain/login/ilogin_repository.dart';
 import 'package:project_2/domain/plants/iplants_repository.dart';
-import 'package:project_2/domain/services/ibase_response.dart';
 import 'package:project_2/domain/services/iuser_service.dart';
 import 'package:project_2/domain/user/imy_user.dart';
 
