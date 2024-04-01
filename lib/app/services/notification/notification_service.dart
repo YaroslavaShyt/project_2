d990c736ab1025edaddf93655a2838bf6bfa458d
YaroslavaShyt <yaroslavashyt@gmail.com>
2024-04-01 20:50:33 +0300

    - unused commit

--- lib/app/services/notification/notification_service.dart
+++ lib/app/services/notification/notification_service.dart
@@ -2,7 +2,6 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:awesome_notifications_fcm/awesome_notifications_fcm.dart';
 import 'package:flutter/material.dart';
 import 'package:project_2/app/routing/inavigation_util.dart';
-import 'package:project_2/app/routing/routes.dart';
 import 'package:project_2/app/utils/deep_linking/deep_link_handler.dart';
 
 class NotificationService {
